c33a931fab0bc39ad991fb07e2b3292fe329e3f2
David Hadka <admin@moeaframework.org>
2012-01-10 02:23:25 +0000

    Adds --epsilon flag to SetHypervolume

--- src/org/moeaframework/analysis/sensitivity/SetHypervolume.java
+++ src/org/moeaframework/analysis/sensitivity/SetHypervolume.java
@@ -3,10 +3,15 @@ package org.moeaframework.analysis.sensitivity;
 import java.io.File;
 
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.moeaframework.core.EpsilonBoxDominanceArchive;
 import org.moeaframework.core.NondominatedPopulation;
 import org.moeaframework.core.PopulationIO;
+import org.moeaframework.core.comparator.EpsilonBoxConstraintComparator;
 import org.moeaframework.core.indicator.Hypervolume;
 import org.moeaframework.util.CommandLineUtility;
+import org.moeaframework.util.TypedProperties;
 
 /**
  * Command line utility for calculating the hypervolume of approximation sets.
@@ -20,11 +25,36 @@ public class SetHypervolume extends CommandLineUtility {
 		super();
 	}
 
+	@SuppressWarnings("static-access")
+	@Override
+	public Options getOptions() {
+		Options options = super.getOptions();
+		
+		options.addOption(OptionBuilder
+				.withLongOpt("epsilon")
+				.hasArg()
+				.withArgName("e1,e2,...")
+				.withDescription("Epsilon values for epsilon-dominance")
+				.create('e'));
+		
+		return options;
+	}
+
 	@Override
 	public void run(CommandLine commandLine) throws Exception {
 		for (String filename : commandLine.getArgs()) {
 			NondominatedPopulation set = new NondominatedPopulation(
 					PopulationIO.readObjectives(new File(filename)));
+			
+			if (commandLine.hasOption("epsilon")) {
+				TypedProperties typedProperties = TypedProperties.withProperty(
+						"epsilon", commandLine.getOptionValue("epsilon"));
+				
+				set = new EpsilonBoxDominanceArchive(
+						new EpsilonBoxConstraintComparator(
+								typedProperties.getDoubleArray("epsilon", 
+										null)), set);
+			}
 			
 			System.out.print(filename);
 			System.out.print(' ');
