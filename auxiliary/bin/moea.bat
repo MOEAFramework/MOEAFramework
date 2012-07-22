7583465785b4df29584c261edb322ff6f48b8196
David Hadka <admin@moeaframework.org>
2012-07-22 22:13:18 +0000

    Removes the currently usued frontend interface

--- auxiliary/bin/moea.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set SCRIPTPATH=%~dp0
-set MOEAFRAMEWORK_HOME=%SCRIPTPATH%..
-set ARGUMENTS=-Xmx1g -server -Djava.ext.dirs=%MOEAFRAMEWORK_HOME%\lib -classpath .
-
-java %ARGUMENTS% org.moeaframework.util.Frontend %*
