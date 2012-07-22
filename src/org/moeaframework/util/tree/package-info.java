06f59be1bef29760a750411202948719792fcc42
David Hadka <admin@moeaframework.org>
2012-07-22 02:22:23 +0000

    Adds copyright header to tree package

--- src/org/moeaframework/util/tree/package-info.java
+++ src/org/moeaframework/util/tree/package-info.java
@@ -15,24 +15,9 @@
  * You should have received a copy of the GNU Lesser General Public License 
  * along with the MOEA Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/**
+ * Classes for representing, building and evaluating expression trees for use
+ * in Genetic Programming (GP).
+ */
 package org.moeaframework.util.tree;
-
-public class Modulus extends Node{
-	
-	public Modulus() {
-		super(Number.class, Number.class, Number.class);
-	}
-	
-	@Override
-	public Modulus copyNode() {
-		return new Modulus();
-	}
-	
-	@Override
-	public Number evaluate(Environment environment) {
-		return NumberArithmetic.mod(
-				(Number)getArgument(0).evaluate(environment),
-				(Number)getArgument(1).evaluate(environment));
-	}
-
-}
