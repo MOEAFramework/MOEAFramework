0935d5782c9c34ccc3464558a3f438c58ac7bbbf
David Hadka <admin@moeaframework.org>
2015-04-08 23:22:04 +0000

    Adds unit test for IndicatorFitnessEvaluator

---
+++ test/org/moeaframework/core/fitness/IndicatorFitnessEvaluatorTest.java
@@ -0,0 +1,54 @@
+package org.moeaframework.core.fitness;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.moeaframework.TestUtils;
+import org.moeaframework.core.FitnessEvaluator;
+import org.moeaframework.core.Population;
+import org.moeaframework.core.Problem;
+import org.moeaframework.problem.MockRealProblem;
+
+/**
+ * Tests the {@link IndicatorFitnessEvaluatorTest} class.
+ */
+public class IndicatorFitnessEvaluatorTest {
+	
+	/**
+	 * Tests if the updated fitness from {@code removeAndUpdate()} equals those
+	 * produced by {@code evaluate} on the resulting population.
+	 */
+	@Test
+	public void testRemoveAndUpdate() {
+		Problem problem = new MockRealProblem();
+		
+		Population population = new Population();
+		population.add(TestUtils.newSolution(0.0, 1.0));
+		population.add(TestUtils.newSolution(0.25, 0.25));
+		population.add(TestUtils.newSolution(0.5, 0.5));
+		population.add(TestUtils.newSolution(0.75, 0.75));
+		population.add(TestUtils.newSolution(1.0, 0.0));
+		
+		IndicatorFitnessEvaluator evaluator = new HypervolumeFitnessEvaluator(problem);
+		evaluator.evaluate(population);
+		evaluator.removeAndUpdate(population, 2);
+		
+		
+		Population expectedPopulation = new Population();
+		expectedPopulation.add(TestUtils.newSolution(0.0, 1.0));
+		expectedPopulation.add(TestUtils.newSolution(0.25, 0.25));
+		expectedPopulation.add(TestUtils.newSolution(0.75, 0.75));
+		expectedPopulation.add(TestUtils.newSolution(1.0, 0.0));
+		
+		evaluator.evaluate(expectedPopulation);
+		
+		
+		Assert.assertEquals(expectedPopulation.size(), population.size());
+		
+		for (int i = 0; i < population.size(); i++) {
+			TestUtils.assertEquals(
+					(Double)population.get(i).getAttribute(FitnessEvaluator.FITNESS_ATTRIBUTE),
+					(Double)expectedPopulation.get(i).getAttribute(FitnessEvaluator.FITNESS_ATTRIBUTE));
+		}
+	}
+
+}
