08e7150f7991c9c7274f89cc8e140b22125affdf
David Hadka <dmh309@arl.psu.edu>
2016-01-13 11:18:31 -0500

    Adds copyright header

--- test/org/moeaframework/analysis/plot/PlotTest.java
+++ test/org/moeaframework/analysis/plot/PlotTest.java
@@ -1,3 +1,20 @@
+/* Copyright 2009-2016 David Hadka
+ *
+ * This file is part of the MOEA Framework.
+ *
+ * The MOEA Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The MOEA Framework is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the MOEA Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.moeaframework.analysis.plot;
 
 import javax.swing.JFrame;
@@ -9,6 +26,10 @@ import org.moeaframework.Instrumenter;
 import org.moeaframework.analysis.collector.Accumulator;
 import org.moeaframework.core.NondominatedPopulation;
 
+/**
+ * Tests the {@link Plot} class.  These tests do not check for the correctness
+ * of the plots, only that the code runs without error.
+ */
 public class PlotTest {
 	
 	public static boolean isJUnitTest() {
