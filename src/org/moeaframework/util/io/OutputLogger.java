8761dca43c96f14c7d6ea2aa2a63fad2e2e7e3ee
Dave Hadka <dhadka@github.com>
2022-11-12 13:04:54 -0600

    Adds remaining types to OutputLogger

--- src/org/moeaframework/util/io/OutputLogger.java
+++ src/org/moeaframework/util/io/OutputLogger.java
@@ -30,24 +30,80 @@ public class OutputLogger implements Closeable {
 		this.writer = writer == null ? System.out : writer;
 	}
 	
-	public void print(String s) {
-		writer.print(s);
+	public void print(boolean b) {
+		writer.print(b);
 	}
 
+	public void print(char c) {
+		writer.print(c);
+	}
+	
+	public void print(char[] cs) {
+		writer.print(cs);
+	}
+	
 	public void print(double d) {
 		writer.print(d);
+	}
+	
+	public void print(float f) {
+		writer.print(f);
+	}
+	
+	public void print(int i) {
+		writer.print(i);
+	}
+	
+	public void print(long l) {
+		writer.print(l);
+	}
+	
+	public void print(Object o) {
+		writer.print(o);
+	}
+	
+	public void print(String s) {
+		writer.print(s);
 	}
 	
 	public void println() {
 		writer.println();
 	}
 	
-	public void println(String s) {
-		writer.println(s);
+	public void println(boolean b) {
+		writer.print(b);
+	}
+
+	public void println(char c) {
+		writer.print(c);
+	}
+	
+	public void println(char[] cs) {
+		writer.print(cs);
 	}
 	
 	public void println(double d) {
-		writer.println(d);
+		writer.print(d);
+	}
+	
+	public void println(float f) {
+		writer.print(f);
+	}
+	
+	public void println(int i) {
+		writer.print(i);
+	}
+	
+	public void println(long l) {
+		writer.print(l);
+	}
+	
+	public void println(Object o) {
+		writer.print(o);
+	}
+	
+	public void println(String s) {
+		writer.println(s);
 	}
 
 	@Override
