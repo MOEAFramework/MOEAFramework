1bc12bb3fe5a5e225e121817296f5200f72ec225
Dave Hadka <dhadka@github.com>
2022-11-21 09:44:48 -0600

    Exclude maven's test dir

--- test/org/moeaframework/algorithm/jmetal/JMetalReferenceTest.java
+++ test/org/moeaframework/algorithm/jmetal/JMetalReferenceTest.java
@@ -36,7 +36,8 @@ public class JMetalReferenceTest {
 	public void testForJMetalReferences() throws IOException {
 		Files.walk(new File("src/").toPath()).forEach((path) -> {
 			if (path.startsWith("src/org/moeaframework/algorithm/jmetal") ||
-					path.startsWith("src/main/java/org/moeaframework/algorithm/jmetal")) {
+					path.startsWith("src/main/java/org/moeaframework/algorithm/jmetal") ||
+					path.startsWith("src/test/")) {
 				return;
 			}
 			
