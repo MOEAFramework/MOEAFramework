33a3a1b4005d452bfbbb7aae4a31fb533b86f95b
Dave Hadka <dhadka@github.com>
2023-04-02 19:17:30 -0500

    Clarify missing JMetal-Plugin in skipped tests

--- test/org/moeaframework/problem/ProblemTest.java
+++ test/org/moeaframework/problem/ProblemTest.java
@@ -35,10 +35,8 @@ public abstract class ProblemTest {
 	 * Call from any test to skip if JMetal does not exist.
 	 */
 	public void assumeJMetalExists() {
-		ProblemFactory.getInstance().forEach(p -> System.out.println(p.getClass().getName()));
-		
-		Assume.assumeTrue(ProblemFactory.getInstance().hasProvider(
-				"org.moeaframework.problem.jmetal.JMetalProblems"));
+		Assume.assumeTrue("JMetal-Plugin required to run test",
+				ProblemFactory.getInstance().hasProvider("org.moeaframework.problem.jmetal.JMetalProblems"));
 	}
 	
 	/**
