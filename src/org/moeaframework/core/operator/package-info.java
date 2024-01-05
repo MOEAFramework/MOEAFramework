dec0849463a7416842f9503af858eea8d19d4426
Dave Hadka <dhadka@github.com>
2024-01-05 09:09:11 -0700

    Add package docs to new selection and initialization packages

--- src/org/moeaframework/core/operator/package-info.java
+++ src/org/moeaframework/core/operator/package-info.java
@@ -17,6 +17,6 @@
  */
 
 /**
- * Package of various selection, mutation, crossover, and hybrid operators.  
+ * Package of various mutation and crossover operators.  
  */
 package org.moeaframework.core.operator;
--- src/org/moeaframework/core/selection/package-info.java
+++ src/org/moeaframework/core/selection/package-info.java
@@ -15,28 +15,8 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with the MOEA Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.moeaframework.core.operator;
-
-import java.util.List;
-
-import org.moeaframework.core.Problem;
-import org.moeaframework.core.Solution;
 
 /**
- * This file has been moved to the initialization package.  Please update your imports accordingly.
- * 
- * @deprecated Moved to {@link org.moeaframework.core.initialization.InjectedInitialization}
+ * Package of various selection operators.  
  */
-@Deprecated
-public class InjectedInitialization extends org.moeaframework.core.initialization.InjectedInitialization {
-
-	public InjectedInitialization(Problem problem, List<Solution> injectedSolutions) {
-		super(problem, injectedSolutions);
-	}
-
-	public InjectedInitialization(Problem problem, Solution... injectedSolutions) {
-		super(problem, injectedSolutions);
-	}
-
-
-}
+package org.moeaframework.core.selection;
