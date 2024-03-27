2a64169f1af1f4f65d0dcef4c738f0115c363ccc
Dave Hadka <dhadka@github.com>
2024-03-27 09:51:32 -0600

    Split UF tests into separate classes

---
+++ test/org/moeaframework/problem/CEC2009/UF2Test.java
@@ -0,0 +1,50 @@
+/* Copyright 2009-2024 David Hadka
+ *
+ * This file is part of the MOEA Framework.
+ *
+ * The MOEA Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The MOEA Framework is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the MOEA Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.moeaframework.problem.CEC2009;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.moeaframework.core.Problem;
+import org.moeaframework.problem.ProblemTest;
+
+public class UF2Test extends ProblemTest {
+	
+	@Test
+	public void test() {
+		Problem problem = new UF2();
+		
+		Assert.assertArrayEquals(new double[] { 2.0, 3.0 }, 
+				evaluateAtLowerBounds(problem).getObjectives(),
+				0.000001);
+		
+		Assert.assertArrayEquals(new double[] { 3.574344, 0.936527 }, 
+				evaluateAtUpperBounds(problem).getObjectives(),
+				0.000001);
+	}
+
+	@Test
+	public void testJMetal() {
+		testAgainstJMetal("UF2");
+	}
+	
+	@Test
+	public void testProvider() {
+		assertProblemDefined("UF2", 2);
+	}
+
+}
