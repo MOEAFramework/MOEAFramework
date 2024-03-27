33a8b34e65d679063378f1213cb4a0d30046ffd5
Dave Hadka <dhadka@github.com>
2024-03-27 09:59:45 -0600

    Split LZ tests into separate classes

---
+++ test/org/moeaframework/problem/LZ/LZ1Test.java
@@ -0,0 +1,52 @@
+/* Copyright 2009-2024 David Hadka
+ *
+ * This file is part of the MOEA Framework.
+ *
+ * The MOEA Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The MOEA Framework is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the MOEA Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.moeaframework.problem.LZ;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.moeaframework.core.Problem;
+import org.moeaframework.problem.ProblemTest;
+
+public class LZ1Test extends ProblemTest {
+	
+	@Test
+	public void test() {
+		Problem problem = new LZ1();
+		
+		Assert.assertArrayEquals(new double[] { 2.0, 3.0 }, 
+				evaluateAtLowerBounds(problem).getObjectives(),
+				0.000001);
+		
+		Assert.assertArrayEquals(new double[] { 1.0, 0.0 }, 
+				evaluateAtUpperBounds(problem).getObjectives(),
+				0.000001);
+	}
+
+	@Test
+	@Ignore("JMetal uses 10 variables instead of 30 so this check isn't compatible")
+	public void testJMetal() {
+		testAgainstJMetal("LZ1");
+	}
+	
+	@Test
+	public void testProvider() {
+		assertProblemDefined("LZ1", 2);
+	}
+
+}
---
+++ test/org/moeaframework/problem/LZ/LZ7Test.java
@@ -0,0 +1,50 @@
+/* Copyright 2009-2024 David Hadka
+ *
+ * This file is part of the MOEA Framework.
+ *
+ * The MOEA Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The MOEA Framework is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the MOEA Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.moeaframework.problem.LZ;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.moeaframework.core.Problem;
+import org.moeaframework.problem.ProblemTest;
+
+public class LZ7Test extends ProblemTest {
+	
+	@Test
+	public void test() {
+		Problem problem = new LZ7();
+		
+		Assert.assertArrayEquals(new double[] { 8.0, 9.0 }, 
+				evaluateAtLowerBounds(problem).getObjectives(),
+				0.000001);
+		
+		Assert.assertArrayEquals(new double[] { 1.0, 0.0 }, 
+				evaluateAtUpperBounds(problem).getObjectives(),
+				0.000001);
+	}
+
+	@Test
+	public void testJMetal() {
+		testAgainstJMetal("LZ7");
+	}
+	
+	@Test
+	public void testProvider() {
+		assertProblemDefined("LZ7", 2);
+	}
+
+}
