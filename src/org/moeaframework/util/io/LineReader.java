2949f77e4ec78fc13c22f8a260a56c103f18221a
David Hadka <dhadka@github.com>
2024-10-22 07:16:30 -0600

    Rename CommentedLineReader to LineReader, add more control over skipping content

---
+++ src/org/moeaframework/util/io/LineReader.java
@@ -0,0 +1,177 @@
+/* Copyright 2009-2024 David Hadka
+ *
+ * This file is part of the MOEA Framework.
+ *
+ * The MOEA Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The MOEA Framework is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the MOEA Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.moeaframework.util.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.moeaframework.core.FrameworkException;
+import org.moeaframework.util.validate.Validate;
+
+/**
+ * Read lines from a character-input stream, with options to ignore blank and commented lines.
+ */
+public class LineReader extends BufferedReader implements Iterable<String>, Iterator<String> {
+	
+	/**
+	 * When set, skip any lines starting with the comment prefix.
+	 */
+	private boolean skipComments;
+	
+	/**
+	 * When set, skip any blank lines that contain only whitespace.
+	 */
+	private boolean skipBlanks;
+	
+	/**
+	 * The prefix string used to identify comment lines.
+	 */
+	private String commentPrefix;
+	
+	/**
+	 * Stores the next line to return when {@link #readLine()} is called.
+	 */
+	private String nextLine;
+
+	/**
+	 * Constructs a new line reader.
+	 * 
+	 * @param in a reader
+	 */
+	public LineReader(Reader in) {
+		super(in);
+		this.commentPrefix = "#";
+	}
+	
+	/**
+	 * Wraps the given reader in a line reader.
+	 * 
+	 * @param reader the reader
+	 * @return the reader wrapped in a line reader
+	 */
+	public static LineReader wrap(Reader reader) {
+		if (reader instanceof LineReader commentedLineReader) {
+			return commentedLineReader;
+		} else {
+			return new LineReader(reader);
+		}
+	}
+	
+	public LineReader skipComments() {
+		this.skipComments = true;
+		return this;
+	}
+	
+	public LineReader skipBlanks() {
+		this.skipBlanks = true;
+		return this;
+	}
+	
+	public LineReader commentPrefix(String commentPrefix) {
+		Validate.that("commentPrefix", commentPrefix).isNotEmpty();
+		
+		this.commentPrefix = commentPrefix;
+		return this;
+	}
+	
+	private boolean skipLine(String line) {
+		if (skipBlanks && line.isBlank()) {
+			return true;
+		}
+		
+		if (skipComments && line.startsWith(commentPrefix)) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private String readLineUncheckedException() {
+		try {
+			return readLine();
+		} catch (IOException e) {
+			throw new FrameworkException("caught exception while itereating over line reader", e);
+		}
+	}
+
+	@Override
+	public String readLine() throws IOException {
+		String line = null;
+		
+		if (nextLine != null) {
+			line = nextLine;
+			nextLine = null;
+		} else {
+			line = super.readLine();
+
+			// loop until we find the next line
+			while ((line != null) && skipLine(line)) {
+				line = super.readLine();
+			}
+
+			return line;
+		}
+		
+		return line;
+	}
+	
+	@Override
+	public Iterator<String> iterator() {
+		return this;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		if (nextLine == null) {
+			nextLine = readLineUncheckedException();
+		}
+		
+		return nextLine != null;
+	}
+
+	@Override
+	public String next() {
+		String result = readLineUncheckedException();
+			
+		if (result == null) {
+			throw new NoSuchElementException();
+		}
+			
+		return result;
+	}
+	
+	@Override
+	public long transferTo(Writer out) throws IOException {
+		String newline = System.lineSeparator();
+		long size = 0;
+
+		for (String line : this) {
+			out.write(line);
+			out.write(newline);
+
+			size += line.length() + newline.length();
+		}
+
+		return size;
+	}
+
+}
---
+++ test/org/moeaframework/util/io/LineReaderTest.java
@@ -0,0 +1,159 @@
+/* Copyright 2009-2024 David Hadka
+ *
+ * This file is part of the MOEA Framework.
+ *
+ * The MOEA Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The MOEA Framework is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the MOEA Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.moeaframework.util.io;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+import org.moeaframework.Assert;
+
+public class LineReaderTest {
+	
+	private final String normalInput = "#comment line\nnon-comment line\n# comment line";
+	
+	private final String commentedInput = "#comment line\n# comment line";
+	
+	private final String blankInput = "   \n   ";
+	
+	private final String emptyInput = "";
+	
+	@Test
+	public void testReadLineKeepingComments() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(normalInput))) {
+			Assert.assertEquals("#comment line", reader.readLine());
+			Assert.assertEquals("non-comment line", reader.readLine());
+			Assert.assertEquals("# comment line", reader.readLine());
+			Assert.assertNull(reader.readLine());
+		}
+	}
+
+	@Test
+	public void testReadLineNormalInput() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(normalInput)).skipComments()) {
+			Assert.assertEquals("non-comment line", reader.readLine());
+			Assert.assertNull(reader.readLine());
+		}
+	}
+	
+	@Test
+	public void testIteratorNormalInput() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(normalInput)).skipComments()) {
+			Assert.assertTrue(reader.hasNext());
+			Assert.assertEquals("non-comment line", reader.next());
+			
+			Assert.assertFalse(reader.hasNext());
+			Assert.assertThrows(NoSuchElementException.class, () -> reader.next());
+		}
+	}
+	
+	@Test
+	public void testTransferToNormalInput() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(normalInput)).skipComments();
+				StringWriter writer = new StringWriter()) {
+			
+			reader.transferTo(writer);
+			
+			Assert.assertEquals("non-comment line" + System.lineSeparator(), writer.toString());
+			Assert.assertNull(reader.readLine());
+		}
+	}
+	
+	@Test
+	public void testReadLineEmptyFile() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(emptyInput))) {
+			Assert.assertNull(reader.readLine());
+		}
+	}
+	
+	@Test
+	public void testIteratorEmptyFile() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(emptyInput))) {
+			Assert.assertFalse(reader.hasNext());
+			Assert.assertThrows(NoSuchElementException.class, () -> reader.next());
+		}
+	}
+	
+	@Test
+	public void testTransferToEmptyFile() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(emptyInput));
+				StringWriter writer = new StringWriter()) {
+			reader.transferTo(writer);
+			Assert.assertEquals("", writer.toString());
+		}
+	}
+	
+	@Test
+	public void testReadLineBlankLine() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(blankInput)).skipBlanks()) {
+			Assert.assertNull(reader.readLine());
+		}
+	}
+	
+	@Test
+	public void testIteratorBlankLine() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(blankInput)).skipBlanks()) {
+			Assert.assertFalse(reader.hasNext());
+			Assert.assertThrows(NoSuchElementException.class, () -> reader.next());
+		}
+	}
+	
+	@Test
+	public void testTransferToBlankLine() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(blankInput)).skipBlanks();
+				StringWriter writer = new StringWriter()) {
+			reader.transferTo(writer);
+			Assert.assertEquals("", writer.toString());
+		}
+	}
+	
+	@Test
+	public void testReadLineEmptyFileWithComment() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(commentedInput)).skipComments()) {
+			Assert.assertNull(reader.readLine());
+		}
+	}
+	
+	@Test
+	public void testIteratorEmptyFileWithComment() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(commentedInput)).skipComments()) {
+			Assert.assertFalse(reader.hasNext());
+			Assert.assertThrows(NoSuchElementException.class, () -> reader.next());
+		}
+	}
+	
+	@Test
+	public void testTransferToEmptyFileWithComment() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(commentedInput)).skipComments();
+				StringWriter writer = new StringWriter()) {
+			reader.transferTo(writer);
+			Assert.assertEquals("", writer.toString());
+		}
+	}
+	
+	@Test
+	public void testWrap() throws IOException {
+		try (LineReader reader = new LineReader(new StringReader(""));
+				LineReader wrappedReader = LineReader.wrap(reader)) {
+			Assert.assertSame(reader, wrappedReader);
+		}
+	}
+
+}
