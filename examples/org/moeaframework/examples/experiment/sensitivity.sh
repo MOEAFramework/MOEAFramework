a71d98778a20705ae0e9f16ab0080e7826adcad9
David Hadka <dhadka@users.noreply.github.com>
2024-11-20 16:35:40 +0000

    List of ignored examples

--- examples/org/moeaframework/examples/experiment/sensitivity.sh
+++ examples/org/moeaframework/examples/experiment/sensitivity.sh
@@ -22,7 +22,7 @@ fi
 java -classpath "lib/*:dist/*" org.moeaframework.analysis.tools.EndOfRunEvaluator \
     --parameterFile examples/org/moeaframework/examples/experiment/NSGAII_Params.txt \
     --input NSGAII_Samples.txt \
-    --output NSGAII_DTLZ2_Resultsres \
+    --output NSGAII_DTLZ2_Results.txt \
     --problem DTLZ2 \
     --algorithm NSGAII \
     --epsilon 0.01
