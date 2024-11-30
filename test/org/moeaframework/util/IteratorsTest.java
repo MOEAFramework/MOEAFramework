c2c7067b9533a2f3d5a847c9ea9d3c820dc54a38
David Hadka <dhadka@github.com>
2024-11-30 02:48:58 -0700

    Add more tests for Iterators

--- test/org/moeaframework/util/IteratorsTest.java
+++ test/org/moeaframework/util/IteratorsTest.java
@@ -17,6 +17,7 @@
  */
 package org.moeaframework.util;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -92,6 +93,38 @@ public class IteratorsTest {
 	}
 	
 	@Test
+	public void testIterableJoin() {
+		Iterator<String> it = Iterators.join(List.of("foo"), List.of("bar")).iterator();
+		
+		Assert.assertTrue(it.hasNext());
+		Assert.assertEquals("foo", it.next());
+		
+		Assert.assertTrue(it.hasNext());
+		Assert.assertEquals("bar", it.next());
+		
+		Assert.assertFalse(it.hasNext());
+		Assert.assertThrows(NoSuchElementException.class, () -> it.next());
+	}
+	
+	@Test
+	public void testRemoveJoin() {
+		List<String> list1 = new ArrayList<>(List.of("foo"));
+		List<String> list2 = new ArrayList<>(List.of("bar"));
+		
+		Iterator<String> it = Iterators.join(list1, list2).iterator();
+		
+		it.next();
+		it.remove();
+		Assert.assertSize(0, list1);
+		Assert.assertSize(1, list2);
+		
+		it.next();
+		it.remove();
+		Assert.assertSize(0, list1);
+		Assert.assertSize(0, list2);
+	}
+	
+	@Test
 	public void testEmptyEnumerate() {
 		Iterator<IndexedValue<String>> it = Iterators.enumerate(Iterators.of());
 		Assert.assertFalse(it.hasNext());
@@ -114,6 +147,38 @@ public class IteratorsTest {
 		
 		Assert.assertFalse(it.hasNext());
 		Assert.assertThrows(NoSuchElementException.class, () -> it.next());
+	}
+	
+	@Test
+	public void testIterableEnumerate() {
+		Iterator<IndexedValue<String>> it = Iterators.enumerate(List.of("first", "second")).iterator();
+		
+		Assert.assertTrue(it.hasNext());
+		IndexedValue<String> first = it.next();
+		Assert.assertEquals(0, first.getIndex());
+		Assert.assertEquals("first", first.getValue());
+		
+		Assert.assertTrue(it.hasNext());
+		IndexedValue<String> second = it.next();
+		Assert.assertEquals(1, second.getIndex());
+		Assert.assertEquals("second", second.getValue());
+		
+		Assert.assertFalse(it.hasNext());
+		Assert.assertThrows(NoSuchElementException.class, () -> it.next());
+	}
+	
+	@Test
+	public void testRemoveEnumerate() {
+		List<String> list = new ArrayList<>(List.of("foo", "bar"));		
+		Iterator<IndexedValue<String>> it = Iterators.enumerate(list).iterator();
+		
+		it.next();
+		it.remove();
+		Assert.assertSize(1, list);
+		
+		it.next();
+		it.remove();
+		Assert.assertSize(0, list);
 	}
 	
 	@Test
@@ -142,6 +207,91 @@ public class IteratorsTest {
 	}
 	
 	@Test
+	public void testIterableZip() {
+		Iterator<Pair<String, String>> it = Iterators.zip(List.of("foo", "hello"), List.of("bar", "world")).iterator();
+		
+		Assert.assertTrue(it.hasNext());
+		Pair<String, String> first = it.next();
+		Assert.assertEquals("foo", first.getKey());
+		Assert.assertEquals("bar", first.getValue());
+		
+		Assert.assertTrue(it.hasNext());
+		Pair<String, String> second = it.next();
+		Assert.assertEquals("hello", second.getKey());
+		Assert.assertEquals("world", second.getValue());
+		
+		Assert.assertFalse(it.hasNext());
+		Assert.assertThrows(NoSuchElementException.class, () -> it.next());
+	}
+	
+	@Test
+	public void testRemoveZip() {
+		List<String> list1 = new ArrayList<>(List.of("foo", "hello"));
+		List<String> list2 = new ArrayList<>(List.of("bar", "world"));
+		
+		Iterator<Pair<String, String>> it = Iterators.zip(list1, list2).iterator();
+		
+		it.next();
+		it.remove();
+		Assert.assertSize(1, list1);
+		Assert.assertSize(1, list2);
+		
+		it.next();
+		it.remove();
+		Assert.assertSize(0, list1);
+		Assert.assertSize(0, list2);
+	}
+	
+	@Test
+	public void testEmptyMap() {
+		Iterator<String> it = Iterators.map(Iterators.of(), x -> x.toString());
+		Assert.assertFalse(it.hasNext());
+		Assert.assertThrows(NoSuchElementException.class, () -> it.next());
+	}
+	
+	@Test
+	public void testMap() {
+		Iterator<String> it = Iterators.map(Iterators.of(1, 2), x -> x.toString());
+		
+		Assert.assertTrue(it.hasNext());
+		Assert.assertEquals("1", it.next());
+		
+		Assert.assertTrue(it.hasNext());
+		Assert.assertEquals("2", it.next());
+		
+		Assert.assertFalse(it.hasNext());
+		Assert.assertThrows(NoSuchElementException.class, () -> it.next());
+	}
+	
+	@Test
+	public void testIterableMap() {
+		Iterator<String> it = Iterators.map(List.of(1, 2), x -> x.toString()).iterator();
+		
+		Assert.assertTrue(it.hasNext());
+		Assert.assertEquals("1", it.next());
+		
+		Assert.assertTrue(it.hasNext());
+		Assert.assertEquals("2", it.next());
+		
+		Assert.assertFalse(it.hasNext());
+		Assert.assertThrows(NoSuchElementException.class, () -> it.next());
+	}
+	
+	@Test
+	public void testRemoveMap() {
+		List<Integer> list = new ArrayList<>(List.of(1, 2));		
+		Iterator<String> it = Iterators.map(list, x -> x.toString()).iterator();
+		
+		it.next();
+		it.remove();
+		Assert.assertSize(1, list);
+		
+		it.next();
+		it.remove();
+		Assert.assertSize(0, list);
+	}
+	
+	@Test
 	public void testEmptyMaterialize() {
 		Iterator<String> it = Iterators.of();
 		Assert.assertEquals(List.of(), Iterators.materialize(it));
@@ -150,6 +300,12 @@ public class IteratorsTest {
 	@Test
 	public void testMaterialize() {
 		Iterator<String> it = Iterators.of("foo", "bar");
+		Assert.assertEquals(List.of("foo", "bar"), Iterators.materialize(it));
+	}
+	
+	@Test
+	public void testIterableMaterialize() {
+		Iterable<String> it = List.of("foo", "bar");
 		Assert.assertEquals(List.of("foo", "bar"), Iterators.materialize(it));
 	}
 	
@@ -166,6 +322,12 @@ public class IteratorsTest {
 	}
 	
 	@Test
+	public void testIterableLast() {
+		Iterable<String> it = List.of("foo", "bar");
+		Assert.assertEquals("bar", Iterators.last(it));
+	}
+	
+	@Test
 	public void testEmptyCount() {
 		Iterator<String> it = Iterators.of();
 		Assert.assertEquals(0, Iterators.count(it));
@@ -174,6 +336,12 @@ public class IteratorsTest {
 	@Test
 	public void testCount() {
 		Iterator<String> it = Iterators.of("foo", "bar");
+		Assert.assertEquals(2, Iterators.count(it));
+	}
+	
+	@Test
+	public void testIterableCount() {
+		Iterable<String> it = List.of("foo", "bar");
 		Assert.assertEquals(2, Iterators.count(it));
 	}
 	
