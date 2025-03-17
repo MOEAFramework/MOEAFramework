07c50b5aac1b62316bc1ac54185f90d2da6b616b
David Hadka <dhadka@github.com>
2025-03-17 09:43:17 -0600

    Handle missing container

--- src/org/moeaframework/analysis/store/fs/HierarchicalFileMap.java
+++ src/org/moeaframework/analysis/store/fs/HierarchicalFileMap.java
@@ -64,27 +64,29 @@ public class HierarchicalFileMap extends FileMap {
 			escapedPathSegments.put(escapedKey, escapedValue);
 		}
 
-		// Match any existing folder structure, though note the same key could appear at multiple locations / depths
-		// within the hierarchy.
-		HierarchicalFileVisitor visitor = new HierarchicalFileVisitor(root, escapedPathSegments);
-		Files.walkFileTree(path, visitor);
-
-		Optional<Path> longestPath = visitor.getMatches().stream()
-				.sorted((x, y) -> -Integer.compare(x.getNameCount(), y.getNameCount()))
-				.findFirst();
-
-		if (longestPath.isPresent()) {
-			path = longestPath.get();
-
-			// If the longest path ended on a key, append the value.  Also remove all keys from path segments.
-			Path relativePath = root.relativize(path);
-
-			if (relativePath.getNameCount() % 2 == 1) {
-				path = path.resolve(escapedPathSegments.get(relativePath.getFileName()));
-			}
-
-			for (int i = 0; i < relativePath.getNameCount(); i += 2) {
-				escapedPathSegments.remove(relativePath.getName(i));
+		if (Files.exists(path)) {
+			// Match any existing folder structure, though note the same key could appear at multiple locations / depths
+			// within the hierarchy.
+			HierarchicalFileVisitor visitor = new HierarchicalFileVisitor(root, escapedPathSegments);
+			Files.walkFileTree(path, visitor);
+	
+			Optional<Path> longestPath = visitor.getMatches().stream()
+					.sorted((x, y) -> -Integer.compare(x.getNameCount(), y.getNameCount()))
+					.findFirst();
+	
+			if (longestPath.isPresent()) {
+				path = longestPath.get();
+	
+				// If the longest path ended on a key, append the value.  Also remove all keys from path segments.
+				Path relativePath = root.relativize(path);
+	
+				if (relativePath.getNameCount() % 2 == 1) {
+					path = path.resolve(escapedPathSegments.get(relativePath.getFileName()));
+				}
+	
+				for (int i = 0; i < relativePath.getNameCount(); i += 2) {
+					escapedPathSegments.remove(relativePath.getName(i));
+				}
 			}
 		}
 
