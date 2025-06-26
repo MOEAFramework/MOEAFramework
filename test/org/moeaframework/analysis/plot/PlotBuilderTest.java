63927eade0e72df312c4ec6350131cc0370b4131
David Hadka <dhadka@github.com>
2025-06-26 08:38:40 -0600

    Add more test coverage for PlotBuilder

--- test/org/moeaframework/analysis/plot/PlotBuilderTest.java
+++ test/org/moeaframework/analysis/plot/PlotBuilderTest.java
@@ -17,15 +17,35 @@
  */
 package org.moeaframework.analysis.plot;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import org.jfree.chart.plot.Plot;
 import org.junit.Test;
 import org.moeaframework.Assert;
 import org.moeaframework.Assume;
+import org.moeaframework.CallCounter;
 import org.moeaframework.TempFiles;
+import org.moeaframework.analysis.plot.style.PaintAttribute;
+import org.moeaframework.analysis.plot.style.PlotAttribute;
+import org.moeaframework.analysis.plot.style.StepsAttribute;
+import org.moeaframework.analysis.plot.style.StyleAttribute;
+import org.moeaframework.analysis.plot.style.ValueAttribute;
+import org.moeaframework.core.Settings;
 
 public class PlotBuilderTest {
+	
+	@Test
+	public void testDefaultDisplayDriver() {
+		Assert.assertNotNull(PlotBuilder.getDisplayDriver());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullDisplayDriver() {
+		PlotBuilder.setDisplayDriver(null);
+	}
 
 	@Test
 	public void testSavePNG() throws IOException {
@@ -49,6 +69,107 @@ public class PlotBuilderTest {
 			.save(tempFile);
 			
 		Assert.assertFileWithContent(tempFile);
+	}
+	
+	@Test
+	public void testToArray() {
+		XYPlotBuilder builder = new XYPlotBuilder();
+		
+		Assert.assertArrayEquals(new double[] { }, builder.toArray(List.of()), Settings.EPS);
+		Assert.assertArrayEquals(new double[] { 5.0, 10.0, 0.5, 1.5 }, builder.toArray(List.of(5, 10L, 0.5f, 1.5)), Settings.EPS);
+	}
+	
+	@Test
+	public void testToArrayWithGetter() {
+		XYPlotBuilder builder = new XYPlotBuilder();
+		
+		Assert.assertArrayEquals(new double[] { }, builder.toArray(List.<Color>of(), c -> c.getRed()), Settings.EPS);
+		Assert.assertArrayEquals(new double[] { 0.0, 255.0 }, builder.toArray(List.of(Color.BLUE, Color.RED), c -> c.getRed()), Settings.EPS);
+	}
+	
+	@Test
+	public void testTo2DArray() {
+		XYPlotBuilder builder = new XYPlotBuilder();
+		
+		Assert.assertEquals(0, builder.to2DArray(List.of()).length);
+		
+		Assert.assertEquals(1, builder.to2DArray(List.of(List.of())).length);
+		Assert.assertEquals(0, builder.to2DArray(List.of(List.of()))[0].length);
+		
+		Assert.assertEquals(new double[][] { { 5.0, 10.0 }, { 0.5, 1.5 } }, builder.to2DArray(List.of(List.of(5, 10L), List.of(0.5f, 1.5))));
+	}
+	
+	@Test
+	public void testApplyStyle() {
+		CallCounter<PlotAttribute> plotAttributeCounter = CallCounter.of(new ValueAttribute<>(5));
+		CallCounter<StyleAttribute> styleAttributeCounter = CallCounter.of(new StyleAttribute() {
+
+			@Override
+			public void apply(Plot plot, int dataset, int series) {
+				System.out.println(plot + " " + dataset + " " + series);
+			}
+			
+		});
+		
+		XYPlotBuilder builder = new XYPlotBuilder();
+		Plot plot = builder.build().getPlot();
+		
+		builder.applyStyle(plot, 0, plotAttributeCounter.getProxy(), styleAttributeCounter.getProxy());
+		builder.applyStyle(plot, 0, 0, plotAttributeCounter.getProxy(), styleAttributeCounter.getProxy());
+		builder.applyStyle(plot, 0, 1, plotAttributeCounter.getProxy(), styleAttributeCounter.getProxy());
+		
+		Assert.assertEquals(0, plotAttributeCounter.getTotalCallCount());
+		Assert.assertEquals(3, styleAttributeCounter.getTotalCallCount());
+	}
+	
+	@Test
+	public void testGetAttribute() {
+		PlotAttribute size = Style.large();
+		PlotAttribute color = Style.blue();
+		
+		XYPlotBuilder builder = new XYPlotBuilder();
+		
+		PaintAttribute attribute1 = builder.getAttribute(PaintAttribute.class);
+		Assert.assertNull(attribute1);
+		
+		PaintAttribute attribute2 = builder.getAttribute(PaintAttribute.class, size);
+		Assert.assertNull(attribute2);
+		
+		PaintAttribute attribute3 = builder.getAttribute(PaintAttribute.class, size, color);
+		Assert.assertNotNull(attribute3);
+		Assert.assertSame(color, attribute3);
+	}
+	
+	@Test
+	public void testHasAttribute() {
+		XYPlotBuilder builder = new XYPlotBuilder();
+		
+		Assert.assertFalse(builder.hasAttribute(PaintAttribute.class));
+		Assert.assertFalse(builder.hasAttribute(PaintAttribute.class, Style.large()));
+		Assert.assertTrue(builder.hasAttribute(PaintAttribute.class, Style.large(), Style.blue()));
+	}
+	
+	@Test
+	public void testGetValueOrDefault() {
+		XYPlotBuilder builder = new XYPlotBuilder();
+		
+		int value1 = builder.getValueOrDefault(StepsAttribute.class, 10);
+		Assert.assertEquals(value1, 10);
+		
+		int value2 = builder.getValueOrDefault(StepsAttribute.class, 10, Style.blue());
+		Assert.assertEquals(value2, 10);
+		
+		int value3 = builder.getValueOrDefault(StepsAttribute.class, 10, Style.blue(), StepsAttribute.of(5));
+		Assert.assertEquals(value3, 5);
+		
+		int value4 = builder.getValueOrDefault(StepsAttribute.class, StepsAttribute.DEFAULT_VALUE);
+		Assert.assertEquals(value4, 100);
+		
+		int value5 = builder.getValueOrDefault(StepsAttribute.class, StepsAttribute.DEFAULT_VALUE, Style.blue());
+		Assert.assertEquals(value5, 100);
+		
+		int value6 = builder.getValueOrDefault(StepsAttribute.class, StepsAttribute.DEFAULT_VALUE, Style.blue(), StepsAttribute.of(5));
+		Assert.assertEquals(value6, 5);
 	}
 
 }
